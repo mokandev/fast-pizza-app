c1ca2d1e5311b05cc28b89ac9ea62730f3f2df69
Carlos Afonso <carlos.flach15@gmail.com>
2024-07-23 22:47:06 -0300

    feat: add router dom and create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./ui/Home"
+import Menu from "./features/menu/Menu"
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/menu',
+    element: <Menu />
+  }
+])
+
 function App() {
-  return (
-    <div>
-      Hello Vite!
-    </div>
-  )
+  return <RouterProvider router={router}/>
 }
 
 export default App
