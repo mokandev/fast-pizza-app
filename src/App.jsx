1a7ba6822fb09e5c10668ef6f5253df6e5376c67
Carlos Afonso <carlos.flach15@gmail.com>
2024-07-23 22:52:20 -0300

    feat: adding more routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import {
 } from "react-router-dom";
 import Home from "./ui/Home"
 import Menu from "./features/menu/Menu"
+import Cart from "./features/cart/Cart"
+import CreateOrder from './features/order/CreateOrder'
+import Order from "./features/order/Order"
 
 const router = createBrowserRouter([
   {
@@ -14,6 +17,18 @@ const router = createBrowserRouter([
   {
     path: '/menu',
     element: <Menu />
+  },
+  {
+    path: '/cart',
+    element: <Cart />
+  },
+  {
+    path: '/order/new',
+    element: <CreateOrder />
+  },
+  {
+    path: '/order/:orderId',
+    element: <Order />
   }
 ])
 
