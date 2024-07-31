e056e32cbde3401cd1e4c5f780677b35abb061bc
Carlos <carlos.flach15@gmail.com>
2024-07-31 10:15:43 -0300

    feat: reusing styles with apply

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -49,13 +49,13 @@ function CreateOrder() {
       <Form method="POST" action="/order/new">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input className='input' type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input className='input' type="tel" name="phone" required />
           </div>
           {formsErrors?.phone && <p>{formsErrors.phone}</p>}
         </div>
@@ -64,7 +64,7 @@ function CreateOrder() {
           <label>Address</label>
           <div>
             <input
-              className="text:sm w-full rounded-full border border-stone-200 px-4 py-2 transition-all duration-100 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6 md:py-3"
+              className="input"
               type="text"
               name="address"
               required
