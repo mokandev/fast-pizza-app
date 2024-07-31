66b00d574cf2bdfc0a35e16b39b498b6bab4997f
Carlos Afonso <carlos.flach15@gmail.com>
2024-07-30 22:41:20 -0300

    feat: styling the form elements

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -63,15 +63,21 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input
+              className="text:sm w-full rounded-full border border-stone-200 px-4 py-2 transition-all duration-100 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6 md:py-3"
+              type="text"
+              name="address"
+              required
+            />
           </div>
         </div>
 
-        <div>
+        <div className="flex items-center gap-5">
           <input
             type="checkbox"
             name="priority"
             id="priority"
+            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,20 +1,24 @@
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const SearchOrder = () => {
-  const [query, setQuery] = useState('')
-  const navigate = useNavigate()
+  const [query, setQuery] = useState('');
+  const navigate = useNavigate();
   const handleSubmit = (e) => {
-    e.preventDefault()
-    if(!query) return
-    navigate(`/order/${query}`)
-    setQuery("")
-
-  }
+    e.preventDefault();
+    if (!query) return;
+    navigate(`/order/${query}`);
+    setQuery('');
+  };
   return (
     <form onSubmit={handleSubmit}>
-      <input placeholder="Search order #" value={query} onChange={(e) => setQuery(e.target.value)} />
+      <input
+        className="text:sm w-[9rem] rounded-full bg-yellow-100 px-4 py-2 transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring-yellow-500 focus:ring-opacity-50 focus:ring-offset-2 sm:w-36 sm:focus:w-72"
+        placeholder="Search order #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
     </form>
-  )
-}
-export default SearchOrder
+  );
+};
+export default SearchOrder;
