a0e7a05cd24c2c3d63ac6ebacc9a70ba0b1710c6
Carlos <carlos.flach15@gmail.com>
2024-08-05 15:25:55 -0300

    feat: styling the menu

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -89,6 +89,7 @@ function CreateOrder() {
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <Button
             disabled={isSubmitting}
+            type="primary"
           >
             {isSubmitting ? 'Placing order...' : 'Order now'}
           </Button>
